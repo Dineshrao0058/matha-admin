1e69bde1efc64a06c5d45c71cbc40ac4dd017247
Dineshrao0058 <dineshrao.vita@gmail.com>
2024-02-08 15:36:42 +0530

    admin login sucess

--- src/app/Admin/admin-login/admin-login.component.ts
+++ src/app/Admin/admin-login/admin-login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { AdminService } from '../../shared/services/admin.service';
 @Component({
   selector: 'app-admin-login',
   standalone: true,
@@ -10,21 +10,33 @@ import { Router } from '@angular/router';
   styleUrl: './admin-login.component.scss'
 })
 export class AdminLoginComponent implements OnInit {
+  AdminLoginForm!: FormGroup;
+  username!: string;
   password!: string;
-  mobileno!: string;
-  loginForm!: FormGroup;
-  constructor(private router: Router, private fb: FormBuilder) {
+  constructor(private AdminApi: AdminService,
+    private fb: FormBuilder,
+    private Routes: Router) { }
 
-  }
   ngOnInit(): void {
-    this.loginForm = this.fb.group({
-      password: ['', Validators.required],
-      mobileno: ['', Validators.required]
-
+    this.AdminLoginForm = this.fb.group({
+     username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
     })
-
   }
-  login() {
+  adminlogin() {
+    if (this.AdminLoginForm.valid) {
+      let username = this.AdminLoginForm.get('username')?.value
+      let password = this.AdminLoginForm.get('password')?.value
 
+      this.AdminApi.getAdmin(this.AdminLoginForm.value).subscribe((res: any) => {
+        if (res) {
+          localStorage.setItem('admin', JSON.stringify(res))
+          this.Routes.navigate(['admin-dashboard'])
+          alert('login sucesfully')
+        } else {
+          alert("Invalid Credentials")
+        }
+      })
+    }
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
-
 import { AdminLoginComponent } from './Admin/admin-login/admin-login.component';
 import { AdminDashboardComponent } from './Admin/admin-dashboard/admin-dashboard.component';
 export const routes: Routes = [
-
+    { path: '', component: AdminLoginComponent },
     { path: 'admin', component: AdminLoginComponent },
     {
-        path: 'dashboard', component: AdminDashboardComponent, children: [
+        path: 'admin-dashboard', component: AdminDashboardComponent, children: [
 
         ]
     }
