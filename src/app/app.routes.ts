30f7e3121b282d43d03921d7c2f5d509e1109fba
Dineshrao0058 <dineshrao.vita@gmail.com>
2024-02-08 17:39:08 +0530

    new changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { AdminLoginComponent } from './Admin/admin-login/admin-login.component';
 import { AdminDashboardComponent } from './Admin/admin-dashboard/admin-dashboard.component';
+import { AddComponent } from './Admin/add/add.component';
 export const routes: Routes = [
     { path: '', component: AdminLoginComponent },
     { path: 'admin', component: AdminLoginComponent },
     {
         path: 'admin-dashboard', component: AdminDashboardComponent, children: [
-
+            
+            { path: 'add', component: AddComponent }
         ]
     }
 ];
