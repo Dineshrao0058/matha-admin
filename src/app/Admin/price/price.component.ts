2bd30f9ab85b452be6a87c43b522b91a753413a8
Dineshrao0058 <dineshrao.vita@gmail.com>
2024-02-28 11:41:45 +0530

    crud changes in price

--- src/app/Admin/price/price.component.ts
+++ src/app/Admin/price/price.component.ts
@@ -13,88 +13,6 @@ import { Router } from '@angular/router';
 })
 export class PriceComponent implements OnInit {
   prices!: any
-  // priceForm!: FormGroup;
-  // size: any;
-  // thickness: any;
-  // orderData: any = [];
-  // grandTotal: number = 0;
-  // income: number = this.grandTotal;
-  // t: any;
-  // s: any;
-  // constructor(private adminApi: AdminService, private fb: FormBuilder) { }
-
-  // ngOnInit(): void {
-  //   this.adminApi.Getprice().subscribe((res: any) => {
-  //     this.size = res;
-  //   });
-
-  //   this.adminApi.Getprice().subscribe((res: any) => {
-  //     this.getPrice = res;
-  //   });
-
-  //   this.priceForm = this.fb.group({
-  //     sizeId: ['', [Validators.required]],
-  //     thicknessId: ['', [Validators.required]],
-  //     price: [null, [Validators.required]],
-
-  //   });
-  // }
-  // get total() {
-  //   return this.s * this.t;
-  // }
-  // getPrice(p: any) {
-  //   let pr = this.size.find((f: any) => f.productname == p.target.value);
-
-  //   this.priceForm.patchValue({
-  //     price: Number(pr.price),
-  //   });
-  // }
-
-  // totalPrice(tt: any) {
-  //   let tot = this.priceForm.value.price * tt.target.value;
-
-  //   this.priceForm.patchValue({
-  //     total: Number(tot),
-  //   });
-  // }
-  // order() {
-  //   this.income += this.grandTotal;
-  //   console.log(this.income, 'ghfj');
-
-  //   this.grandTotal += this.priceForm.value.total;
-  //   if (this.orderData.length === 0) {
-  //     this.orderData = {
-  //       mobilenumber: this.priceForm.value.mobilenumber,
-  //       orderDetails: [
-  //         {
-  //           product: this.priceForm.value.product,
-  //           price: this.priceForm.value.price,
-  //           quantity: this.priceForm.value.quantity,
-  //           total: this.priceForm.value.total,
-  //         },
-  //       ],
-  //       grandTotal: this.grandTotal,
-  //       income: this.income,
-  //     };
-  //   } else {
-  //     let ordersProduct = {
-  //       product: this.priceForm.value.product,
-  //       price: this.priceForm.value.price,
-  //       quantity: this.priceForm.value.quantity,
-  //       total: this.priceForm.value.total,
-  //     };
-
-  //     this.orderData.orderDetails.push(ordersProduct);
-  //     this.orderData.grandTotal = this.grandTotal;
-  //     this.orderData.income = this.income;
-  //   }
-  //   console.log(this.orderData, 'od');
-
-  //   this.priceForm.reset();
-  // }
-  // clear() {
-
-  // }
   addPricesform!: FormGroup
   sizes: any
   thickness: any
@@ -139,6 +57,8 @@ export class PriceComponent implements OnInit {
     this.editbtn = true
     this.addPricesform.patchValue({
       price: p.price,
+      sizeId: p.sizeId,
+      thicknessId: p.thicknessId,
       id: p._id
     })
   }
